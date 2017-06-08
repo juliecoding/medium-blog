d629af6612ed3765007f73784f9f048e190ac8b7
juliecoding <julie.akohler@gmail.com>
2017-06-08 16:12:12 -0600

    Style homepage

---
+++ public/app/backgroundDirective.js
@@ -0,0 +1,12 @@
+angular
+  .module('app')
+  .directive('backImg', function() {
+    return function(scope, element, attrs) {
+      attrs.$observe('backImg', function(value) {
+        element.css({
+          'background-image': 'url(' + value + ')',
+          'background-size': 'cover'
+        });
+      });
+    };
+  })
